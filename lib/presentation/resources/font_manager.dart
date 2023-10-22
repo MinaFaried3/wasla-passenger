3b163082f25e77a71ebbf2bd71f82dfb693ddf6d
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2023-10-22 23:19:06 +0300

    add cairo arabic font to app

--- lib/presentation/resources/font_manager.dart
+++ lib/presentation/resources/font_manager.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 class FontConstants {
-  static const String fontFamily = "";
+  static const String fontFamily = "cairo";
 }
 
 class FontWeightManager {
+  static const FontWeight extraLight = FontWeight.w200;
   static const FontWeight light = FontWeight.w300;
   static const FontWeight regular = FontWeight.w400;
   static const FontWeight medium = FontWeight.w500;
   static const FontWeight semiBold = FontWeight.w600;
   static const FontWeight bold = FontWeight.w700;
-  static const FontWeight ultraBold = FontWeight.w900;
+  static const FontWeight extraBold = FontWeight.w800;
+  static const FontWeight black = FontWeight.w900;
 }
 
 class FontSize {
