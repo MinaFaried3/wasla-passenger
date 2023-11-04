31ed5a4fa10c8f1f0ba9f69550537571d0e317be
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2023-11-04 23:52:59 +0200

    merge to main

--- lib/presentation/resources/managers/strings_manager.dart
+++ lib/presentation/resources/managers/strings_manager.dart
@@ -4,4 +4,18 @@ class AppStrings {
 
   static const String skip = 'skip';
   static const String login = 'login';
+
+  static const String onboardingTitle1 = 'onboarding_title_1';
+  static const String onboardingTitle2 = 'onboarding_title_2';
+  static const String onboardingTitle3 = 'onboarding_title_3';
+  static const String onboardingTitle4 = 'onboarding_title_4';
+  static const String onboardingTitle5 = 'onboarding_title_5';
+  static const String onboardingTitle6 = 'onboarding_title_6';
+
+  static const String onboardingSubTitle1 = 'onboarding_sub_title_1';
+  static const String onboardingSubTitle2 = 'onboarding_sub_title_2';
+  static const String onboardingSubTitle3 = 'onboarding_sub_title_3';
+  static const String onboardingSubTitle4 = 'onboarding_sub_title_4';
+  static const String onboardingSubTitle5 = 'onboarding_sub_title_5';
+  static const String onboardingSubTitle6 = 'onboarding_sub_title_5';
 }
